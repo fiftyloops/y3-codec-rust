72dce5c63e7e08e421ae14cad1a5d1422a66fb24
fiftyloops <zhifeig@uw.edu>
2021-08-25 16:58:16 +0800

    new data type: varint

---
+++ codec/src/varint.rs
@@ -0,0 +1,131 @@
+use crate::Codec;
+
+// int32
+
+pub fn size_of_varint32(value: i32) -> usize {
+    size_of_varint(value as i64, 32)
+}
+
+impl Codec {
+    pub fn encode_varint32(&mut self, buffer: &mut Vec<u8>, value: i32) -> Result<(), &str> {
+        self.encode_varint(buffer, value as i64)
+    }
+
+    pub fn decode_varint32(&mut self, buffer: Vec<u8>) -> Result<i32, &str> {
+        match self.decode_varint(buffer) {
+            Ok(value) => Ok(value as i32),
+            Err(msg) => Err(msg),
+        }
+    }
+}
+
+// uint32
+
+pub fn size_of_varuint32(value: u32) -> usize {
+    size_of_varint(value as i32 as i64, 32)
+}
+
+impl Codec {
+    pub fn encode_varuint32(&mut self, buffer: &mut Vec<u8>, value: u32) -> Result<(), &str> {
+        self.encode_varint(buffer, value as i32 as i64)
+    }
+
+    pub fn decode_varuint32(&mut self, buffer: Vec<u8>) -> Result<u32, &str> {
+        match self.decode_varint(buffer) {
+            Ok(value) => Ok(value as u32),
+            Err(msg) => Err(msg),
+        }
+    }
+}
+
+// int64
+
+pub fn size_of_varint64(value: i64) -> usize {
+    size_of_varint(value, 64)
+}
+
+impl Codec {
+    pub fn encode_varint64(&mut self, buffer: &mut Vec<u8>, value: i64) -> Result<(), &str> {
+        self.encode_varint(buffer, value)
+    }
+
+    pub fn decode_varint64(&mut self, buffer: Vec<u8>) -> Result<i64, &str> {
+        self.decode_varint(buffer)
+    }
+}
+
+// uint64
+
+pub fn size_of_varuint64(value: u64) -> usize {
+    size_of_varint(value as i64, 64)
+}
+
+impl Codec {
+    pub fn encode_varuint64(&mut self, buffer: &mut Vec<u8>, value: u64) -> Result<(), &str> {
+        self.encode_varint(buffer, value as i64)
+    }
+
+    pub fn decode_varuint64(&mut self, buffer: Vec<u8>) -> Result<u64, &str> {
+        match self.decode_varint(buffer) {
+            Ok(value) => Ok(value as u64),
+            Err(msg) => Err(msg),
+        }
+    }
+}
+
+// general
+
+fn size_of_varint(value: i64, bits: usize) -> usize {
+    if bits != 32 && bits != 64 {
+        panic!("bits must be either 32 or 64");
+    }
+    let prefix = value >> (bits - 1);
+    let mut size = bits / 8 - 1;
+    while size > 0 {
+        let look_ahead = value >> (8 * size - 1);
+        if look_ahead != prefix {
+            return size + 1;
+        }
+        size -= 1;
+    }
+    1
+}
+
+impl Codec {
+    pub fn encode_varint(&mut self, buffer: &mut Vec<u8>, value: i64) -> Result<(), &str> {
+        if self.size == 0 {
+            return Err("nothing to encode");
+        }
+        while self.size > 0 {
+            if self.ptr >= buffer.len() {
+                return Err("insufficient buffer size");
+            }
+            self.size -= 1;
+            buffer[self.ptr] = (value >> (8 * self.size)) as u8;
+            self.ptr += 1;
+        }
+        Ok(())
+    }
+
+    pub fn decode_varint(&mut self, buffer: Vec<u8>) -> Result<i64, &str> {
+        if self.size == 0 {
+            return Err("nothing to encode");
+        }
+        let mut value: i64 = 0;
+        if self.size > 0 {
+            if self.ptr >= buffer.len() {
+                return Err("insufficient buffer size");
+            }
+            value = (buffer[self.ptr] >> 7) as i64;
+        }
+        while self.size > 0 {
+            if self.ptr >= buffer.len() {
+                return Err("insufficient buffer size");
+            }
+            self.size -= 1;
+            value = (value << 8) | (buffer[self.ptr]) as i64;
+            self.ptr += 1;
+        }
+        Ok(value)
+    }
+}
---
+++ codec/tests/varint.rs
@@ -0,0 +1,134 @@
+use codec::Codec;
+use codec::varint;
+
+#[test]
+fn int32() {
+    test_int32(-1, vec![0xFF]);
+    test_int32(-5, vec![0xFB]);
+    test_int32(63, vec![0x3F]);
+    test_int32(-65, vec![0xBF]);
+    test_int32(127, vec![0x7F]);
+    test_int32(255, vec![0x00, 0xFF]);
+    test_int32(-4097, vec![0xEF, 0xFF]);
+    test_int32(-8193, vec![0xDF, 0xFF]);
+    test_int32(-2097152, vec![0xE0, 0x00, 0x00]);
+    test_int32(-134217729, vec![0xF7, 0xFF, 0xFF, 0xFF]);
+    test_int32(-2147483648, vec![0x80, 0x00, 0x00, 0x00]);
+}
+
+#[test]
+fn uint32() {
+    test_uint32(1, vec![0x01]);
+    test_uint32(127, vec![0x7F]);
+    test_uint32(128, vec![0x00, 0x80]);
+    test_uint32(130, vec![0x00, 0x82]);
+    test_uint32(1048576, vec![0x10, 0x00, 0x00]);
+    test_uint32(134217728, vec![0x08, 0x00, 0x00, 0x00]);
+    test_uint32(4294967295, vec![0xFF]);
+}
+
+#[test]
+fn int64() {
+    test_int64(0, vec![0x00]);
+    test_int64(1, vec![0x01]);
+    test_int64(-1, vec![0xFF]);
+}
+
+#[test]
+fn uint64() {
+    test_uint64(0, vec![0x00]);
+    test_uint64(1, vec![0x01]);
+    test_uint64(18446744073709551615, vec![0xFF]);
+}
+
+fn test_int32(value: i32, bytes: Vec<u8>) {
+    let size = varint::size_of_varint32(value);
+    assert_eq!(size, bytes.len());
+    
+    let mut buffer: Vec<u8> = Vec::with_capacity(size);
+    let mut codec = Codec {
+        ptr:    0,
+        size:   size,
+    };
+    match codec.encode_varint32(&mut buffer, value) {
+        Ok(_) => {
+            for i in 0..size {
+                assert_eq!(buffer[i], bytes[i]);
+            }
+        },
+        Err(msg) => println!("{}", msg),
+    }
+    match codec.decode_varint32(buffer) {
+        Ok(decoder_output) => assert_eq!(decoder_output, value),
+        Err(msg) => println!("{}", msg),
+    }
+}
+
+fn test_uint32(value: u32, bytes: Vec<u8>) {
+    let size = varint::size_of_varuint32(value);
+    assert_eq!(size, bytes.len());
+    
+    let mut buffer: Vec<u8> = Vec::with_capacity(size);
+    let mut codec = Codec {
+        ptr:    0,
+        size:   size,
+    };
+    match codec.encode_varuint32(&mut buffer, value) {
+        Ok(_) => {
+            for i in 0..size {
+                assert_eq!(buffer[i], bytes[i]);
+            }
+        },
+        Err(msg) => println!("{}", msg),
+    }
+    match codec.decode_varuint32(buffer) {
+        Ok(decoder_output) => assert_eq!(decoder_output, value),
+        Err(msg) => println!("{}", msg),
+    }
+}
+
+fn test_int64(value: i64, bytes: Vec<u8>) {
+    let size = varint::size_of_varint64(value);
+    assert_eq!(size, bytes.len());
+    
+    let mut buffer: Vec<u8> = Vec::with_capacity(size);
+    let mut codec = Codec {
+        ptr:    0,
+        size:   size,
+    };
+    match codec.encode_varint64(&mut buffer, value) {
+        Ok(_) => {
+            for i in 0..size {
+                assert_eq!(buffer[i], bytes[i]);
+            }
+        },
+        Err(msg) => println!("{}", msg),
+    }
+    match codec.decode_varint64(buffer) {
+        Ok(decoder_output) => assert_eq!(decoder_output, value),
+        Err(msg) => println!("{}", msg),
+    }
+}
+
+fn test_uint64(value: u64, bytes: Vec<u8>) {
+    let size = varint::size_of_varuint64(value);
+    assert_eq!(size, bytes.len());
+    
+    let mut buffer: Vec<u8> = Vec::with_capacity(size);
+    let mut codec = Codec {
+        ptr:    0,
+        size:   size,
+    };
+    match codec.encode_varuint64(&mut buffer, value) {
+        Ok(_) => {
+            for i in 0..size {
+                assert_eq!(buffer[i], bytes[i]);
+            }
+        },
+        Err(msg) => println!("{}", msg),
+    }
+    match codec.decode_varuint64(buffer) {
+        Ok(decoder_output) => assert_eq!(decoder_output, value),
+        Err(msg) => println!("{}", msg),
+    }
+}
